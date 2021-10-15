3280a434c2c1daad6cafd2c750520691f0f62eb4
epswartz <eswartzen@gmail.com>
2021-10-14 22:26:35 -0400

    add app engine

---
+++ app_engine/main.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import requests
+from flask import Flask, request, send_file
+from token_creator import create_token
+import io
+import time
+from PIL import Image
+from skimage.io import imsave
+
+app = Flask(__name__)
+
+@app.route('/create_token',methods=['POST'])
+def creator():
+  file = request.files['file']
+  data = file.read()
+  # convert string of image data to uint8
+  arr = np.fromstring(data, np.uint8)
+
+  # decode image
+  img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+  token = create_token(img)
+  now_str = str(round(time.time() * 10000))
+  output_filename = "output_" + now_str + ".png"
+
+  # imsave(output_filename, token)
+  # return "Image Saved: " + output_filename
+
+  print("Returning token")
+  buf = io.BytesIO()
+  token_img = Image.fromarray(token)
+  token_img.save(buf, format='PNG')
+  buf.seek(0)
+  return send_file(buf, attachment_filename=output_filename, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ app_engine/token_creator.py
@@ -0,0 +1,75 @@
+from predict import container_predict
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from skimage.io import imread
+from glob import glob
+from tqdm.notebook import tqdm
+import numpy as np
+from PIL import Image
+import uuid
+
+TOKEN_TEMPLATE = imread("./token_template.png")
+TEMPLATE_COLOR = np.empty((280,280,4))
+TEMPLATE_COLOR[:,:,0] = 254
+TEMPLATE_COLOR[:,:,1] = 0
+TEMPLATE_COLOR[:,:,2] = 254
+TEMPLATE_COLOR[:,:,3] = 255
+mask = np.all(TEMPLATE_COLOR == TOKEN_TEMPLATE, axis=2)
+
+
+def create_token(img):
+    img_id = str(uuid.uuid4())
+    resp = container_predict(img, img_id)
+    box = resp['predictions'][0]['detection_boxes'][0] # TODO multiple faces
+    token = crop_token(img, box)
+    return token
+
+def create_token_batch(imgs):
+    img_ids = [str(uuid.uuid4()) for img in imgs]
+    resp = container_predict(imgs, img_ids)
+    tokens = []
+    for i in range(len(imgs)):
+        box = resp['predictions'][i]['detection_boxes'][0] # TODO multiple faces
+        tokens.append(crop_token(imgs[i], box))
+    return tokens
+
+
+def crop_token(img, box):
+
+    h = img.shape[0]
+    w = img.shape[1]
+    y1, x1, y2, x2 = box
+
+    y1 -= .03
+    y2 += .03
+    x1 -= .03
+    x2 += .03
+
+    rectx = round(x1*w)
+    recty = round(y1*h)
+    rectw = round((x2*w) - rectx)
+    recth = round((y2*h) - recty)
+
+    if recth < rectw:
+        diff = rectw - recth
+        recty -= diff//2
+        recth = rectw
+    elif rectw < recth:
+        diff = recth - rectw
+        rectx -= diff//2
+        rectw = recth
+
+    rectx1 = max(0, rectx)
+    recty1 = max(0, recty)
+    rectx2 = min(img.shape[1]-1, rectx+rectw)
+    recty2 = min(img.shape[0]-1, recty+recth)
+
+    cropped = Image.fromarray(img[recty1:recty2, rectx1:rectx2,:])
+    cropped = np.array(cropped.resize((280,280)))
+    if cropped.shape[-1] == 3:
+        cropped = np.dstack((cropped, np.empty((280,280))))
+    cropped[:,:,3] = 255
+
+    token = TOKEN_TEMPLATE.copy()
+    token[mask] = cropped[mask]
+    return token
